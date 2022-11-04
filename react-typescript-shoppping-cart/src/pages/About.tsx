258a96fa6b212adece5ae813344cfca4c6ac143b
alvin zhang <88403531+C-137-9527@users.noreply.github.com>
2022-11-04 15:21:35 -0700

    Add files via upload

---
+++ react-typescript-shoppping-cart/src/pages/About.tsx
@@ -0,0 +1,10 @@
+export function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>
+        This app is built with Typescript, React, React-Router, and BootStrap.
+      </p>
+    </>
+  );
+}
